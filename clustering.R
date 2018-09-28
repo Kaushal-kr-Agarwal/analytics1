1cdd32da7feee44392fc157148244a2a54eb76b5
Kaushal-kr-Agarwal <kaushal.agarwal16@gmail.com>
2018-09-28 23:23:48 +0530

    1123

---
+++ clustering.R
@@ -0,0 +1,20 @@
+#clustering
+
+set.seed(1234)
+subject1=trunc(rnorm(30,mean = 60, sd = 15))
+range(subject1)
+subject1
+
+marks=data.frame(subject1)
+head(marks)
+
+k2=kmeans(marks, centers = 2)
+k2
+k2$size
+marks[k2$cluster==1,]
+marks[k2$cluster==2,]
+mean(marks[k2$cluster==1,])
+mean(marks[k2$cluster==2,])
+k2$centers
+length(marks[k2$cluster==1,])
+length(marks[k2$cluster==2,])
